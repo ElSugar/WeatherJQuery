2af3de890e8a982627e28d25ab8ab3e7d3ba5bab
Elizabeth <elizabeth.saharova@mail.ru>
2020-04-11 20:59:52 +0300

    html+css+js

--- script.js
+++ script.js
@@ -1,12 +1,31 @@
 $(document).ready(function() {
-  $.get(
-    "http://api.openweathermap.org/data/2.5/weather",
-    {
-      "id": "524894",
-      "appid": "f616afccbe8d4877daef3e9161357d4a",
-    },
-    function(data) {
-      console.log(data);
-    }
-  )
-})
+  $.getJSON('current.city.list.json', function(data) {
+    $('select').on('change', function() {
+      var out='';
+      for (var key in data) {
+        if (data[key].country==$('select option:selected').val()) {
+          out += `<p value="${data[key].id}">${data[key].name}</p>`;
+        }
+      }
+      $('#city').html(out);
+      $('#city p').on('click', function(){
+        $.get(
+          "api.openweathermap.org/data/2.5/weather",
+          {
+            "id": $(this).attr('value'),
+            "appid": "f616afccbe8d4877daef3e9161357d4a"
+          },
+            function(data) {
+              let out='';
+              out += 'Weather: <b>'+data.weather[0].main+'</b><br>';
+              out += '<p style="text-align:center"><img src="openweathermap.org/img/w/'+data.weather[0].icon+'.png"></p>';
+              out += 'Temprature: <b>'+Math.round(data.main.temp-273)+'&#176;C</b><br>';
+              out += 'Humidity: <b>'+data.main.humidity+'%</b><br>';
+              console.log(data.main);
+              $('#weather').html(out);
+            }
+        );
+      });
+    });
+  });
+});
